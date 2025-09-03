0d92996e98cf66d3140d489b4b6f725ff91a977f
Bunyod <bunyodabduxamidov0@gmail.com>
2025-09-03 18:38:03 +0500

    js10

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 const ranglar = document.querySelectorAll(".rang_card");
 const cards = document.querySelectorAll(".card");
 const colors = ["blue","red","green","yellow"]
-let rangla = 'white'
-ranglar.forEach((rang, i) => {
-  rang.addEventListener("click", () => {
-    cards.forEach(card=>{
-      rangla = colors[i]
-      card.style =`background-color : ${colors[i]}`}
-    );
-  });
-});
-const shakllar = document.querySelectorAll(".shakl_box div");
-
-
 const clipaths = [
   "polygon(50% 0%, 0% 100%, 100% 100%)", 
   "circle(50% at 50% 50%)",              
   "inset(0)",                            
   "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)" // s4
 ];
+let clippath = clipaths[2]
+let rangla = 'white'
+ranglar.forEach((rang, i) => {
+  rang.addEventListener("click", () => {
+    cards.forEach(card=>{
+      rangla = colors[i]
+      card.style =`background-color : ${colors[i]};clip-path : ${clippath}`
+    });
+  });
+});
+const shakllar = document.querySelectorAll(".shakl_box div");
 
 shakllar.forEach((shakl, i) => {
   shakl.addEventListener("click", () => {
     cards.forEach(card => {
-        card.style = `background-color : ${rangla};clip-path : ${clipaths[i]}`
+      clippath = clipaths[i]
+      card.style = `background-color : ${rangla};clip-path : ${clipaths[i]}`
     });
   });
 });
